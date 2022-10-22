72a3524e47b40734e6e17b736c0c5a9ef8756926
sumi <17628385085@163.com>
2022-10-22 13:30:35 +0800

    10.19.arcgis2

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,8 @@ namespace _10._12.arcgis1
 {
     public partial class Form1 : Form
     {
+        public ILayer pCurLayar { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
@@ -59,7 +61,6 @@ namespace _10._12.arcgis1
 
         private void axMapControleye_OnMouseMove(object sender, IMapControlEvents2_OnMouseMoveEvent e)
         {
-            axMapControleye.Extent = axMapControleye.FullExtent;
             if (e.button == 1)//左键点击移动，俩个视图联动
             {
                 IPoint pPoint = new PointClass();
@@ -107,7 +108,7 @@ namespace _10._12.arcgis1
             pColor.Transparency = 255;
             //边界线
             ILineSymbol pOutline = new SimpleLineSymbolClass();
-            pOutline.Width = 3;
+            pOutline.Width = 2;
             pOutline.Color = pColor;
             //symbol的背景色
             pColor = new RgbColorClass();
@@ -120,6 +121,94 @@ namespace _10._12.arcgis1
             pFillSymbol.Color = pColor;
             pFillSymbol.Outline = pOutline;
             return pFillSymbol;
+        }
+
+        private void axMapControleye_OnMouseDown(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+            IPoint pPoint = new PointClass();
+            pPoint.PutCoords(e.mapX, e.mapY);
+            axMapControlmain.CenterAt(pPoint);
+            axMapControlmain.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
+        }
+
+        private void axMapControlmain_OnMouseMove(object sender, IMapControlEvents2_OnMouseMoveEvent e)
+        {
+            toolStripStatusLabel1.Text = e.mapX.ToString(".###")+@"  "+e.mapY.ToString(".###");
+        }
+
+        private void axMapControlmain_OnMouseDown(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+            IMap pMap= axMapControlmain.Map;;
+            IActiveView pActiveView = pMap as IActiveView;
+            IPoint pt= axMapControlmain.ToMapPoint(e.x, e.y);
+            IMarkerElement pMarkerElement= new MarkerElementClass();
+            IElement pElement= pMarkerElement as IElement;
+            pElement.Geometry = pt;
+            IGraphicsContainer pGraphicsContainer= pMap as IGraphicsContainer;
+            pGraphicsContainer.AddElement((IElement)pMarkerElement, 0);
+            pActiveView.Refresh();
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < axMapControlmain.LayerCount-1; i++)
+            {
+                axMapControlmain.get_Layer(i).Visible = true;
+            }
+            axMapControlmain.Refresh();
+            axMapControleye.Refresh();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < axMapControlmain.LayerCount - 1; i++)
+            {
+                axMapControlmain.get_Layer(i).Visible = false;
+            }
+            axMapControlmain.Refresh();
+            axMapControleye.Refresh();
+        }
+
+        private void axTOCControl1_OnMouseDown(object sender, ITOCControlEvents_OnMouseDownEvent e)
+        {
+            if (e.button == 2)
+            {
+                esriTOCControlItem itemType=esriTOCControlItem.esriTOCControlItemNone;
+                IBasicMap basicMap=null;
+                ILayer layer=null;
+                object unk=null, data=null;
+                axTOCControl1.HitTest(e.x, e.y, ref itemType,ref basicMap,ref layer,ref unk,ref data);
+                if (layer !=null)
+                {
+                    pCurLayar = layer;
+                    contextMenuStrip2.Show(MousePosition.X, MousePosition.Y);
+                }
+                else if (basicMap !=null)
+                {
+                    contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+                }
+                
+
+            }
+        }
+
+        private void 关闭ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pCurLayar !=null)
+            {
+                pCurLayar.Visible = false;
+                (axMapControlmain.Map as IActiveView).PartialRefresh(esriViewDrawPhase.esriViewGeography, pCurLayar, null);
+            }
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pCurLayar != null)
+            {
+                pCurLayar.Visible = true;
+                (axMapControlmain.Map as IActiveView).PartialRefresh(esriViewDrawPhase.esriViewGeography, pCurLayar, null);
+            }
         }
     }
 }

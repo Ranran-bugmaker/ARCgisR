e12af8d37434c2ae9410548e00e928409e668bd7
sumiQAQ <80933462+sumiQAQ@users.noreply.github.com>
2022-11-06 20:53:17 +0800

    修改查询逻辑

--- Form2.cs
+++ Form2.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using _10._12.arcgis1;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using ESRI.ArcGIS.Carto;
+﻿using ESRI.ArcGIS.Carto;
 using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace _10._12.arcgis1
 {
     public partial class Form2 : Form
     {
-        private static int selectedindex=-1;
+        private static IGeometry seageometry { get; set; }
+        private static int selectedindex = -1;
         public static int Selectedindex { get { return selectedindex; } private set { selectedindex = value; } }
         public Form2()
         {
@@ -129,52 +124,6 @@ namespace _10._12.arcgis1
         }
 
 
-        /// <summary>
-        /// 根据已配置的查询条件来执行属性查询操作。
-        /// </summary>
-        private void SelectFeaturesByAttribute()
-        {
-            //使用FeatureLayer对象的IFeatureSelection接口来执行查询操作。这里有一个接口转换操作。
-            IFeatureSelection featureSelection = currentFeatureLayer as IFeatureSelection;
-            //新建IQueryFilter接口的对象来进行where语句的定义
-            IQueryFilter queryFilter = new QueryFilterClass();
-            //设置where语句内容
-            queryFilter.WhereClause = wheretxt.Text;
-            //通过接口转换使用Map对象的IActiveView接口来部分刷新地图窗口，从而高亮显示查询的结果
-            IActiveView activeView = currentMap as IActiveView;
-
-            //根据查询选择方式的不同，得到不同的选择集
-            switch (comboBoxSelectMethod.SelectedIndex)
-            {
-                //在新建选择集的情况下
-                case 0:
-                    //首先使用IMap接口的ClearSelection()方法清空地图选择集
-                    currentMap.ClearSelection();
-                    //根据定义的where语句使用IFeatureSelection接口的SelectFeatures方法选择要素，并将其添加到选择集中
-                    featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultNew, false);
-                    break;
-                //添加到当前选择集的情况
-                case 1:
-                    featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultAdd, false);
-                    break;
-                //从当前选择集中删除的情况
-                case 2:
-                    featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultXOR, false);
-                    break;
-                //从当前选择集中选择的情况
-                case 3:
-                    featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultAnd, false);
-                    break;
-                //默认为新建选择集的情况
-                default:
-                    currentMap.ClearSelection();
-                    featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultNew, false);
-                    break;
-            }
-
-            //部分刷新操作，只刷新地理选择集的内容
-            activeView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, activeView.Extent);
-        }
 
         private void listBoxFields_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -205,49 +154,6 @@ namespace _10._12.arcgis1
 
             try
             {
-                //！！！！！！！！仅限于数据源来自于地理数据库
-                ////使用FeatureClass对象的IDataset接口来获取dataset和workspace的信息
-                //IDataset dataset = (IDataset)currentFeatureLayer.FeatureClass;
-                ////使用IQueryDef接口的对象来定义和查询属性信息。通过IWorkspace接口的CreateQueryDef()方法创建该对象。
-                //IQueryDef queryDef = ((IFeatureWorkspace)dataset.Workspace).CreateQueryDef();
-                ////设置所需查询的表格名称为dataset的名称
-                //queryDef.Tables = dataset.Name;
-                ////设置查询的字段名称。可以联合使用SQL语言的关键字，如查询唯一值可以使用DISTINCT关键字。
-                //queryDef.SubFields = "DISTINCT (" + currentFieldName + ")";     //这里特别要注意，IQueryDef接口只能用于查询ArcSDE、个人和文件地理数据库数据
-                //                                                                //queryDef.WhereClause= dataset.Name+"="+ currentFieldName;
-
-
-                ////使用IField接口获取当前所需要使用的字段的信息
-                //IFields fields = currentFeatureLayer.FeatureClass.Fields;
-                //IField field = fields.get_Field(fields.FindField(currentFieldName));
-
-
-                ////执行查询并返回ICursor接口的对象来访问整个结果的集合
-                //ICursor cursor = queryDef.Evaluate();
-
-
-                ////对整个结果集合进行遍历，从而添加所有的唯一值
-                ////使用IRow接口来操作结果集合。首先定位到第一个查询结果。
-                //IRow row = cursor.NextRow();        //也可以使用IFeatureCursor进行遍历
-                //                                    //如果查询结果非空，则一直进行添加操作
-                //while (row != null)
-                //{
-                //    ;
-                //    //对String类型的字段，唯一值的前后添加'和'，以符合SQL语句的要求
-                //    if (field.Type == esriFieldType.esriFieldTypeString)
-                //    {
-                //        listBoxValues.Items.Add("\'" + row.Value[0].ToString() + "\'");
-                //    }
-                //    else
-                //    {
-                //        listBoxValues.Items.Add(row.Value[0].ToString());
-                //    }
-                //    //继续执行下一个结果的添加
-                //    row = cursor.NextRow();
-                //}
-
-
-
                 // 属性过滤器
                 IQueryFilter pQueryFilter = new QueryFilter();
                 pQueryFilter.AddField(currentFieldName);
@@ -286,7 +192,7 @@ namespace _10._12.arcgis1
 
         private void listBoxValues_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            wheretxt.Text+= listBoxValues.SelectedItem.ToString();
+            wheretxt.Text += listBoxValues.SelectedItem.ToString();
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -300,30 +206,85 @@ namespace _10._12.arcgis1
             catch { }
         }
 
+        /// <summary>
+        /// 根据已配置的查询条件来执行属性查询操作。
+        /// </summary>
+        private void SelectFeaturesByAttribute()
+        {
+            if (seageometry == null)
+            {
+                //使用FeatureLayer对象的IFeatureSelection接口来执行查询操作。这里有一个接口转换操作。
+                IFeatureSelection featureSelection = currentFeatureLayer as IFeatureSelection;
+                //新建IQueryFilter接口的对象来进行where语句的定义
+                IQueryFilter queryFilter = new QueryFilterClass();
+                //设置where语句内容
+                queryFilter.WhereClause = wheretxt.Text;
+                //通过接口转换使用Map对象的IActiveView接口来部分刷新地图窗口，从而高亮显示查询的结果
+                IActiveView activeView = currentMap as IActiveView;
+
+                //根据查询选择方式的不同，得到不同的选择集
+                switch (comboBoxSelectMethod.SelectedIndex)
+                {
+                    //在新建选择集的情况下
+                    case 0:
+                        //首先使用IMap接口的ClearSelection()方法清空地图选择集
+                        currentMap.ClearSelection();
+                        //根据定义的where语句使用IFeatureSelection接口的SelectFeatures方法选择要素，并将其添加到选择集中
+                        featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultNew, false);
+                        break;
+                    //添加到当前选择集的情况
+                    case 1:
+                        featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultAdd, false);
+                        break;
+                    //从当前选择集中删除的情况
+                    case 2:
+                        featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultXOR, false);
+                        break;
+                    //从当前选择集中选择的情况
+                    case 3:
+                        featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultAnd, false);
+                        break;
+                    //默认为新建选择集的情况
+                    default:
+                        currentMap.ClearSelection();
+                        featureSelection.SelectFeatures(queryFilter, esriSelectionResultEnum.esriSelectionResultNew, false);
+                        break;
+                }
+
+                //部分刷新操作，只刷新地理选择集的内容
+                activeView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, activeView.Extent);
+            }
+            else
+            {
+                //定义空间查询过滤器对象
+                ISpatialFilter pSpatialFilter = new SpatialFilterClass();
+                //设置sql查询语句
+                pSpatialFilter.WhereClause = wheretxt.Text;
+                //设置查询范围
+                pSpatialFilter.Geometry = seageometry;
+                //给定范围与查询对象的空间关系
+                pSpatialFilter.SpatialRel = esriSpatialRelEnum.esriSpatialRelContains;
+
+                IFeatureSelection featureSelection = currentFeatureLayer as IFeatureSelection;
+                IActiveView activeView = currentMap as IActiveView;
+
+                //首先使用IMap接口的ClearSelection()方法清空地图选择集
+                currentMap.ClearSelection();
+                //根据定义的where语句使用IFeatureSelection接口的SelectFeatures方法选择要素，并将其添加到选择集中
+                featureSelection.SelectFeatures(pSpatialFilter, esriSelectionResultEnum.esriSelectionResultNew, false);
+
+                activeView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, activeView.Extent);
+            }
+
+
+        }
         private void button5_Click(object sender, EventArgs e)
         {
-            //定义空间查询过滤器对象
-            ISpatialFilter pSpatialFilter = new SpatialFilterClass();
-            //设置sql查询语句
-            pSpatialFilter.WhereClause = wheretxt.Text;
-            //设置查询范围
             if (Form1.geometry == null)
             {
                 return;
             }
-            pSpatialFilter.Geometry = Form1.geometry;
-            //给定范围与查询对象的空间关系
-            pSpatialFilter.SpatialRel = esriSpatialRelEnum.esriSpatialRelContains;
-
-            IFeatureSelection featureSelection = currentFeatureLayer as IFeatureSelection;
-            IActiveView activeView = currentMap as IActiveView;
-
-            //首先使用IMap接口的ClearSelection()方法清空地图选择集
-            currentMap.ClearSelection();
-            //根据定义的where语句使用IFeatureSelection接口的SelectFeatures方法选择要素，并将其添加到选择集中
-            featureSelection.SelectFeatures(pSpatialFilter, esriSelectionResultEnum.esriSelectionResultNew, false);
-
-            activeView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, activeView.Extent);
+            seageometry = Form1.geometry;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
